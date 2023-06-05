1a325f85cd9a948fd1a467cb1fec2d000de8bc84
jonathankerth <jonathanpkerth@gmail.com>
2023-06-05 14:53:43 -0700

    add atatus monitoring

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
+import * as atatus from "atatus-spa";
+atatus.config("2bd662d1e28a4254a3af12842614eb94").install();
+
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
