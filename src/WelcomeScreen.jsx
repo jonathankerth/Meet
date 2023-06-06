4ab8abe3cdb3ff02b5988c6efd39be00b3c1b164
jonathankerth <jonathanpkerth@gmail.com>
2023-06-06 12:57:08 -0700

    clean up welcome screen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -7,27 +7,19 @@ function WelcomeScreen(props) {
 			<h4>
 				Log in to see upcoming events around the world for full-stack developers
 			</h4>
-			<div className="button_cont" align="center">
-				<div class="google-btn">
-					<div class="google-icon-wrapper">
-						<img
-							class="google-icon"
-							src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
-o.svg"
-							alt="Google sign-in"
-						/>
-					</div>
-					<button
-						onClick={() => {
-							props.getAccessToken();
-						}}
-						rel="nofollow noopener"
-						class="btn-text"
-					>
-						<b>Sign in with google</b>
-					</button>
-				</div>
+
+			<div className="button_cont" style={{ textAlign: "center" }}>
+				<button
+					onClick={() => {
+						props.getAccessToken();
+					}}
+					rel="nofollow noopener"
+					class="btn-text"
+				>
+					<b>Sign in with google</b>
+				</button>
 			</div>
+
 			<a
 				href="https://github.com/jonathankerth/meet/blob/main/public/Meet%20Privacy%20Policy.html"
 				rel="nofollow noopener"
