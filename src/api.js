045eec9d25e5b11d5db15aa0be547d20e77cc0d5
jonathankerth <jonathanpkerth@gmail.com>
2023-06-05 21:00:01 -0700

    remove getauth URL /

--- src/api.js
+++ src/api.js
@@ -59,7 +59,7 @@ export const getAccessToken = async () => {
 		const code = await searchParams.get("code");
 		if (!code) {
 			const results = await axios.get(
-				"https://zb7siwyfe7.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url/"
+				"https://zb7siwyfe7.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url"
 			);
 			const { authUrl } = results.data;
 			return (window.location.href = authUrl);
