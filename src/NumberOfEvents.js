3585dcfc96366955ef32d46c5d828201762305b3
jonathankerth <jonathanpkerth@gmail.com>
2023-06-06 12:30:27 -0700

    Revert "update numberofevents"
    This reverts commit a8301e67d71ef627c1e140a3094c00a12fb8b668.

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,91 +1,51 @@
 import React, { Component } from "react";
-import "./App.css";
-import CitySearch from "./CitySearch";
-import EventList from "./EventList";
-import NumberOfEvents from "./NumberOfEvents";
-import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
-import "./nprogress.css";
-import WelcomeScreen from "./WelcomeScreen";
+import { ErrorAlert } from "./Alert";
 
-class App extends Component {
-	state = {
-		events: [],
-		locations: [],
-		numberOfEvents: 32,
-		showWelcomeScreen: undefined,
-	};
+class NumberOfEvents extends Component {
+	constructor() {
+		super();
+		this.state = {
+			numberOfEvents: 32,
+			errorText: "",
+		};
+	}
 
-	updateEvents = (location, eventCount) => {
-		getEvents().then((events) => {
-			let locationEvents = events.filter(
-				(event) => event.location === location
-			);
-
-			if (eventCount !== undefined) {
-				this.setState({ numberOfEvents: eventCount });
-			}
-
-			locationEvents = locationEvents.slice(0, this.state.numberOfEvents);
-
+	handleInputChanged = (event) => {
+		const value = event.target.value;
+		if (value >= 1 && value <= 32) {
 			this.setState({
-				events: locationEvents,
+				numberOfEvents: value,
+				errorText: "",
 			});
-		});
+		} else {
+			this.setState({
+				numberOfEvents: value,
+				errorText: "Please enter a valid number between 1 and 32",
+			});
+		}
 	};
 
-	async componentDidMount() {
-		this.mounted = true;
-		const accessToken = localStorage.getItem("access_token");
-		const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-		const searchParams = new URLSearchParams(window.location.search);
-		const code = await searchParams.get("code");
-		this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-		if ((code || isTokenValid) && this.mounted) {
-			getEvents().then((events) => {
-				if (this.mounted) {
-					this.setState({
-						events: events,
-						locations: extractLocations(events),
-					});
-				}
-			});
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.numberOfEvents !== this.state.numberOfEvents) {
+			this.props.updateEvents(this.state.numberOfEvents);
 		}
 	}
 
-	componentWillUnmount() {
-		this.mounted = false;
-	}
-
 	render() {
-		if (this.state.showWelcomeScreen === undefined)
-			return <div className="App" />;
 		return (
-			<div className="App">
-				<WelcomeScreen
-					showWelcomeScreen={this.state.showWelcomeScreen}
-					getAccessToken={() => {
-						getAccessToken();
-					}}
+			<div className="numberOfEvents">
+				<ErrorAlert text={this.state.errorText} />
+				<input
+					type="number"
+					className="numberOfEvents"
+					min={1}
+					max={32}
+					value={this.state.numberOfEvents}
+					onChange={this.handleInputChanged}
 				/>
-				{!this.state.showWelcomeScreen && (
-					<>
-						<CitySearch
-							locations={this.state.locations}
-							updateEvents={this.updateEvents}
-						/>
-						<NumberOfEvents
-							numberOfEvents={this.state.numberOfEvents}
-							updateEvents={this.updateEvents}
-						/>
-						<EventList
-							events={this.state.events}
-							numberOfEvents={this.state.numberOfEvents}
-						/>
-					</>
-				)}
 			</div>
 		);
 	}
 }
 
-export default App;
+export default NumberOfEvents;
