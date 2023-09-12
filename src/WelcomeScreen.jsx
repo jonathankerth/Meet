4b5e93f34548326ba3559200a9e3d15f2714ff9f
jonathankerth <jonathanpkerth@gmail.com>
2023-09-12 11:38:10 -0700

    improve styling

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,32 +1,28 @@
 import React from 'react'
 import './WelcomeScreen.css'
+
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ? (
     <div className="WelcomeScreen">
-      <h1>Welcome to the Meet app</h1>
-      <h4>
-        Log in to see upcoming events around the world for full-stack developers
-      </h4>
-
-      <div className="button_cont" style={{ textAlign: 'center' }}>
-        <button
-          onClick={() => {
-            props.getAccessToken()
-          }}
-          rel="nofollow noopener"
-          class="btn-text"
-        >
-          <b>Sign in with google</b>
+      <div className="content">
+        <h1>Welcome to the Meet app</h1>
+        <h4>
+          Discover upcoming events for full-stack developers around the world
+          built using google oAuth2.0, AWS Lambda, and the Google Calendar API.
+        </h4>
+        <button className="login-button" onClick={props.getAccessToken}>
+          Sign in with Google
         </button>
       </div>
-
       <a
         href="https://github.com/jonathankerth/meet/blob/main/public/Meet%20Privacy%20Policy.html"
         rel="nofollow noopener"
+        className="privacy-link"
       >
-        Privacy policy
+        Privacy Policy
       </a>
     </div>
   ) : null
 }
+
 export default WelcomeScreen
